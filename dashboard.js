e0d901987d2d18daa3b369a7e7a6bb540e80b7af
Adam Byers <adam@abyers.ca>
2024-11-30 17:35:52 -0500

    Created new main layout and card constructor

---
+++ dashboard.js
@@ -0,0 +1,56 @@
+/**
+ * 
+ * @constructor
+ * 
+ * @param {string} id - A unique ID that wil be used for the card's content.
+ *                    - The recommended format is a shortened form of the card name.
+ * 
+ * @param {string} name The name that should be displayed on the card in the dashboard
+ * 
+ * @param {function(string): void} builder - A function that will generate the chart in the card using the string supplied for the chart location ID.
+ *                                         - This function will run last so include any interactivity in this as well using the ID as a prefix.
+ * 
+ * @param {function(string): string=} cardContent - A function that will create HTML content that will be displayed in the text section of the card.
+ *                                                - Use the string supplied as an ID for any content that must be accessed through the builder function.
+ * 
+ * @property {string} id The unique prefix ID of the card content.
+ * @property {string} name The name that is displayed on the card.
+ * @property {function(): void} createCard The function that will create the card itself.
+ */
+export function ChartCard(id, name, builder, cardContent) {
+  this.id = id
+  this.name = name;
+  this.builder = builder;
+  if (cardContent) this.cardContent = cardContent;
+
+  this.createCard = function() {
+    const card = d3
+      .select("#dashboardCards")
+      .append("div")
+      .attr("class", "card card-xl");
+
+    const cardVisual = card
+      .append("div")
+      .attr("class", "card-visual")
+      .attr("id", `${this.id}Chart`);
+
+    const cardData = card
+      .append("div")
+      .attr("class", "card-data");
+
+    cardData
+      .append("h2")
+      .text(this.name);
+    
+    if (this.cardContent) {
+      cardData
+        .html(this.cardContent(this.id));
+    }
+    
+    console.log("")
+
+    this.builder(`${this.id}Chart`);
+
+  }
+
+}
